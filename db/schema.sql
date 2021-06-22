e52de63d7367918f341f09dc6e55298c19fe9aac
Jonathan Liu <jonathan.liu1@student.unsw.edu.au>
2021-06-22 17:11:36 +1000

    Add test account, fix errors

--- db/schema.sql
+++ db/schema.sql
@@ -5,8 +5,12 @@ create or replace table Users (
     first_name text not null,
     last_name text not null,
     password text not null, -- TODO salt + hash instead of plaintext...
-    profile_pic_path text not null, -- path in flask server to the profile pic file
+    profile_pic_path text, -- path in flask server to the profile pic file, NULL if no pic
     password_reset_code integer, -- password reset code if user requests password, NULL if not
                                 -- TODO salt + hash instead of plaintext...
-    primary key (serial)
+    primary key (user_id)
 );
+
+-- temporary test account
+insert ignore into table Users(email, first_name, last_name, password, profile_pic_path, password_reset_code) 
+    values ('test@test.com', 'Test', 'Account', 'testing', NULL, NULL);
