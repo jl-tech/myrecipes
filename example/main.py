82d406aa02030d10658d4e36a17fa23ca6794583
Junxuan Zhang <z5221478@ad.unsw.edu.au>
2021-06-24 00:01:18 +0800

    basic interaction

--- example/main.py
+++ example/main.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
@@ -8,6 +8,21 @@ app = Flask(__name__)
 def hello_world():
     return render_template('index.html',url='123')
 
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if request.method == 'GET':
+        return render_template('login.html')
+
+    else:
+        usern = request.form.get('username')
+        passw = request.form.get('password')
+
+        if usern == 'username' and passw == '123456':
+            return redirect('http://127.0.0.1:5000/')
 
 if __name__ == '__main__':
     app.run()
