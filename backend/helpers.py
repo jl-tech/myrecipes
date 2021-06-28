3411f12a2ff2a68ee5d0a68cc9233ae95af9556b
jl-tech <jonathan.liu1@student.unsw.edu.au>
2021-06-28 21:56:55 +1000

    Host image on remote server instead of attaching so is compatible with Outlook

--- backend/helpers.py
+++ backend/helpers.py
@@ -13,21 +13,19 @@ def send_email(subject, message_html, message_plain, dest_addr, banner_image_pat
     message["From"] = "myrecipes.supp@gmail.com"
     message["To"] = dest_addr
 
-    message_alternative = MIMEMultipart('alternative')
-    message.attach(message_alternative)
 
     if banner_image_path is not None:
-        message_html = '<center><br><img src="cid:image0", style="max-width: 80%; height: auto;"><br></center>\n' + message_html
+        message_html = f'<center><br><img src="{banner_image_path}", style="max-width: 80%; height: auto;"><br></center>\n' + message_html
 
-    message_alternative.attach(MIMEText(message_plain, "plain"))
-    message_alternative.attach(MIMEText(message_html, "html"))
+    message.attach(MIMEText(message_plain, "plain"))
+    message.attach(MIMEText(message_html, "html"))
 
-    file = open(banner_image_path, 'rb')
-    image = MIMEImage(file.read())
-    file.close()
-
-    image.add_header('Content-ID', '<image0>')
-    message.attach(image)
+    # file = open(banner_image_path, 'rb')
+    # image = MIMEImage(file.read())
+    # file.close()
+    #
+    # image.add_header('Content-ID', '<image0>')
+    # message.attach(image)
 
     ctxt = ssl.create_default_context()
     try:
