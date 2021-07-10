56a28455e49dad6e197c9dbe8a6060a69a99566f
Junxuan Zhang <z5221478@ad.unsw.edu.au>
2021-07-10 13:44:45 +0800

    debugged

--- backend/search.py
+++ backend/search.py
@@ -32,15 +32,21 @@ def do_search(name, type, serving_size, ingredients, step_key_words):
     for result in results:
         recipe_id = result['recipe_id']
         meet_requirement = False
+
+        if len(ingredients) is 0:
+            meet_requirement = True
         for ingredient in ingredients:
             query = "select * from RecipeIngredients where recipe_id=%s AND ingredient_name=%s"
             cur.execute(query, (recipe_id, ingredient,))
             if len(cur.fetchall()) != 0:
                 meet_requirement = True
                 break
+
         if meet_requirement is False:
             continue
 
+        if len(step_key_words) is 0:
+            return_recipe.append(recipe_id)
         for key_word in step_key_words:
             query = "select * from RecipeSteps where recipe_id=%s AND step_text LIKE %s"
             cur.execute(query, (recipe_id, "%"+key_word+"%",))
@@ -48,8 +54,8 @@ def do_search(name, type, serving_size, ingredients, step_key_words):
                 return_recipe.append(recipe_id)
                 break
 
-    json_ary = []
-    for id in return_recipe:
-        json_ary.append(recipe.get_recipe_details(id))
+    ary = []
+    for recipe_id in return_recipe:
+        ary.append(recipe.get_recipe_details(recipe_id))
 
-    return json_ary
+    return ary
