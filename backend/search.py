bfc7962d6c677b819fd71799925144a3656ac102
Junxuan Zhang <z5221478@ad.unsw.edu.au>
2021-07-10 12:30:10 +0800

    search template

--- backend/search.py
+++ backend/search.py
@@ -12,4 +12,23 @@ import tokenise
 import sys
 
 def do_search(name, type, serving_size, ingredients, key_word):
+    query_lock.acquire()
+    cur = con.cursor()
+
+    if name is None:
+        name = "%"
+    if type is None:
+        type = "%"
+    if serving_size is None:
+        serving_size = "%"
+
+    query = "select * from Recipes where name LIKE %s AND type LIKE %s AND serving_size LIKE %s"
+    cur.execute(query, (name, type, serving_size,))
+    result = cur.fetchall()
+
+
+
+
+
     return 1;
+
