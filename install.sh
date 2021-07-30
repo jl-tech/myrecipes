bed7a17a379c07c27a8b51de848db9043b914b2b
jl-tech <jonathan.liu1@student.unsw.edu.au>
2021-07-30 23:31:17 +1000

    Remove shell restart after install and fix dump

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ printf "Welcome to MyRecipes install script\n"
 printf "This will take up to 10 minutes. Press Ctrl+C in the next 5 seconds to cancel.\n"
 sleep 5
 
-printf "Beginning MyRecipes install..."
-printf "[0/3] Running apt-get update..."
+printf "Beginning MyRecipes install...\n"
+printf "[0/3] Running apt-get update...\n"
 sleep 1
 apt-get update
 printf "[0/3] Installing database completed.\n\n\n"
@@ -15,7 +15,7 @@ sleep 3
 apt-get -y install mysql-server
 service mysql start
 mysql < ./db/schema.sql
-mysql -p myrecipes < dump.sql
+mysql -p myrecipes < ./db/dump.sql
 printf "[1/3] Installing database completed.\n\n\n"
 
 printf "[2/3] Installing MyRecipes backend...\n"
@@ -31,9 +31,7 @@ apt-get -y install npm
 npm install --prefix ./frontend_react ./frontend_react
 printf "[3/3] Installing frontend completed.\n\n\n"
 
-printf "Install complete! Restarting shell in 5 seconds (this is necessary to complete the install)\n"
-sleep 5
-exec "$SHELL"
+printf "Install complete!"
 
 
 
